bfcbee07094d79ad05bb07248e1b7ee90d2d41a7
Novemser <novemser@gmail.com>
2017-10-22 02:11:04 +0800

    [Bug fix]现在每次取Level都是辛德勒

--- server/src/content/level.go
+++ server/src/content/level.go
@@ -12,49 +12,53 @@ type Level struct {
 	GlobalContext  *e.GlobalContext
 }
 
-var Level1 = &Level{
-	"教程",
-	"魔鬼般的赋值语句",
-	"欢迎来到葫芦娃的王国。在这里,7只英勇的葫芦娃将与诡计多端的白骨精展开一场计算机科学的较量……",
-	[]*e.ThreadContext{
-		e.DefaultThreadContext(
-			0, &[]e.Instruction{
-				e.NewAssignmentInstruction("a",
-					e.NewAdditionExpression(
-						e.NewVariableExpression("a"),
-						e.NewLiteralExpression(1),
+func GetLevel(id int) *Level {
+	return &Level{
+		"教程",
+		"魔鬼般的赋值语句",
+		"欢迎来到葫芦娃的王国。在这里,7只英勇的葫芦娃将与诡计多端的白骨精展开一场计算机科学的较量……",
+		[]*e.ThreadContext{
+			e.DefaultThreadContext(
+				0, &[]e.Instruction{
+					e.NewAssignmentInstruction("a",
+						e.NewAdditionExpression(
+							e.NewVariableExpression("a"),
+							e.NewLiteralExpression(1),
+						),
 					),
-				),
-				e.NewStartIfStatement(
-					e.NewEqualityExpression(
-						e.NewVariableExpression("a"),
-						e.NewLiteralExpression(1),
-					), "if",
-				),
-				e.NewCriticalSectionExpression(),
-				e.NewEndIfStatement("if"),
-			},
-		),
-		e.DefaultThreadContext(
-			1, &[]e.Instruction{
-				e.NewAssignmentInstruction("a",
-					e.NewAdditionExpression(
-						e.NewVariableExpression("a"),
-						e.NewLiteralExpression(1),
+					e.NewStartIfStatement(
+						e.NewEqualityExpression(
+							e.NewVariableExpression("a"),
+							e.NewLiteralExpression(1),
+						), "if",
 					),
-				),
-				e.NewStartIfStatement(
-					e.NewEqualityExpression(
-						e.NewVariableExpression("a"),
-						e.NewLiteralExpression(1),
-					), "if",
-				),
-				e.NewCriticalSectionExpression(),
-				e.NewEndIfStatement("if"),
-			},
-		),
-	},
-	e.NewGlobalContext(e.Pair{
-		Key:"a", Value:e.GlobalStateType{"a", "a", 0},
-	}),
+					e.NewCriticalSectionExpression(),
+					e.NewEndIfStatement("if"),
+				},
+			),
+			e.DefaultThreadContext(
+				1, &[]e.Instruction{
+					e.NewAssignmentInstruction("a",
+						e.NewAdditionExpression(
+							e.NewVariableExpression("a"),
+							e.NewLiteralExpression(1),
+						),
+					),
+					e.NewStartIfStatement(
+						e.NewEqualityExpression(
+							e.NewVariableExpression("a"),
+							e.NewLiteralExpression(1),
+						), "if",
+					),
+					e.NewCriticalSectionExpression(),
+					e.NewEndIfStatement("if"),
+				},
+			),
+		},
+		e.NewGlobalContext(e.Pair{
+			Key:"a", Value:e.GlobalStateType{"a", "a", 0},
+		}),
+	}
 }
+
+var Level1 = GetLevel(1)
