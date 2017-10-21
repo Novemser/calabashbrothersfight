10b0d612453a843d27b98d7ea019d466a21dff99
yokoyang <yokocc255@163.com>
2017-10-21 13:35:24 +0800

    [add] dataType

--- server/src/gameState.go
+++ server/src/gameState.go
@@ -1,1 +1,14 @@
 package main
+
+
+type GameState struct {
+	threadState int
+	level int
+	globalState int
+}
+
+
+
+func (g *GameState) ResetForLevel(level Level) {
+
+}
--- server/src/level.go
+++ server/src/level.go
@@ -1,1 +1,10 @@
 package main
+type Level struct {
+	id string
+	name string
+	shortDescription string
+	longDescription string
+	victoryText string
+	threads string
+	variables string
+}
